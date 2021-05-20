2466835b2e8f192ff90d0a21c4ed17c6a6a71d83
rafaelalbia <rafaelalbia@outlook.com>
2021-05-20 01:36:17 -0300

    Java Spring initial import

---
+++ infotag/src/main/java/com/infotag/infotag/InfotagApplication.java
@@ -0,0 +1,13 @@
+package com.infotag.infotag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfotagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfotagApplication.class, args);
+	}
+
+}
---
+++ infotag/src/test/java/com/infotag/infotag/InfotagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotag.infotag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfotagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
